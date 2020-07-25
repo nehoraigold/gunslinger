d7f6201358ca7ae76b4400d65c2e08f07fb27a8b
Ori Gold <nehoraigold@gmail.com>
2020-07-25 17:38:17 +0300

    World: Added class

---
+++ src/World.py
@@ -0,0 +1,25 @@
+import typing
+from src.Room import Room
+from src import Utils
+
+
+class World:
+    def __init__(self, map_csv_file_path: str, room_descriptions_csv_file_path: str):
+        self.board: typing.Dict[typing.Tuple[int, int], Room] = self.load_board_from_csv(map_csv_file_path)
+        self.initialize_rooms(room_descriptions_csv_file_path)
+
+    @staticmethod
+    def load_board_from_csv(csv_file_path: str) -> typing.Dict[typing.Tuple[int, int], Room]:
+        return {(i, j): Room(cell) for j, row in enumerate(Utils.load_csv(csv_file_path)) for i, cell in enumerate(row)}
+
+    def initialize_rooms(self, room_descriptions_csv_file_path: str):
+        ROOM_NAME_INDEX = 0
+        ROOM_DESCRIPTION_INDEX = 1
+        for row in Utils.load_csv(room_descriptions_csv_file_path):
+            for room in self.board.values():
+                if str(room) == row[ROOM_NAME_INDEX]:
+                    room.SetDescription(row[ROOM_DESCRIPTION_INDEX])
+                    break
+
+    def GetRoom(self, coordinate: typing.Tuple[int, int]) -> Room:
+        return self.board.get(coordinate, None)
---
+++ tests/world_tests/get_room_world_tests.py
@@ -0,0 +1,47 @@
+import unittest
+from src.World import World
+from tests.utils_tests.load_csv_tests import create_csv_file, delete_csv_file
+
+
+class WorldGetRoomTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.map_file_path = "tests/world_tests/world_map.csv"
+        self.room_descriptions_file_path = "tests/world_tests/room_desc.csv"
+        self.map_data = [["Room1", "Room2"],
+                         ["Room3", "Room4"]]
+        self.room_descriptions_data = [["Room1", "Description for room 1."],
+                                       ["Room2", "Description for room 2."],
+                                       ["Room3", "Description for room 3."],
+                                       ["Room4", "Description for room 4."]]
+        create_csv_file(self.map_file_path, self.map_data)
+        create_csv_file(self.room_descriptions_file_path, self.room_descriptions_data)
+
+    def tearDown(self) -> None:
+        delete_csv_file(self.map_file_path)
+        delete_csv_file(self.room_descriptions_file_path)
+
+    def test_get_valid_room_correct_name(self):
+        coord = (1, 1)
+        room_name = self.map_data[coord[0]][coord[1]]
+        world = World(self.map_file_path, self.room_descriptions_file_path)
+
+        room = world.GetRoom(coord)
+
+        self.assertEqual(room_name, str(room))
+
+    def test_get_valid_room_correct_description(self):
+        coord = (1, 0)
+        description = self.room_descriptions_data[1][1]
+
+        world = World(self.map_file_path, self.room_descriptions_file_path)
+        room = world.GetRoom(coord)
+
+        self.assertIn(description, room.Describe())
+
+    def test_get_room_invalid_coordinate(self):
+        invalid_coord = (5, 7)
+
+        world = World(self.map_file_path, self.room_descriptions_file_path)
+        room = world.GetRoom(invalid_coord)
+
+        self.assertIsNone(room)
