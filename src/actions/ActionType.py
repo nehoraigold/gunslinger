6156b7d7953f6edcb25ebfe03b8844c61e65d8bd
Ori Gold <nehoraigold@gmail.com>
2020-07-27 00:10:47 +0300

    Added more ActionTypes in preparation for future development

--- src/actions/ActionType.py
+++ src/actions/ActionType.py
@@ -4,4 +4,9 @@ import enum
 class ActionType(enum.Enum):
     MOVE = "MOVE"
     LOOK = "LOOK"
+    TAKE = "TAKE"
+    DROP = "DROP"
+    INVENTORY = "INVENTORY"
+    INTERACTION = "INTERACTION"
+    HELP = "HELP"
     QUIT = "QUIT"
