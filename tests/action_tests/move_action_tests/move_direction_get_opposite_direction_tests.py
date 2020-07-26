d218d6447a855a60358e01f10545872cf04ef6a2
Ori Gold <nehoraigold@gmail.com>
2020-07-26 11:15:34 +0300

    MoveDirection: Added GetOppositeDirection function

---
+++ tests/action_tests/move_action_tests/move_direction_get_opposite_direction_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from src.actions.action_data.move.MoveDirection import MoveDirection
+
+
+class MoveDirectionGetOppositeDirectionTests(unittest.TestCase):
+    def test_get_opposite_direction_of_left(self):
+        self.assertEqual(MoveDirection.GetOppositeDirection(MoveDirection.LEFT), MoveDirection.RIGHT)
+
+    def test_get_opposite_direction_of_right(self):
+        self.assertEqual(MoveDirection.GetOppositeDirection(MoveDirection.RIGHT), MoveDirection.LEFT)
+
+    def test_get_opposite_direction_of_up(self):
+        self.assertEqual(MoveDirection.GetOppositeDirection(MoveDirection.UP), MoveDirection.DOWN)
+
+    def test_get_opposite_direction_of_down(self):
+        self.assertEqual(MoveDirection.GetOppositeDirection(MoveDirection.DOWN), MoveDirection.UP)
