94f8e1aab6a93251dabc98a4bc357cbf104474d1
Ori Gold <nehoraigold@gmail.com>
2020-08-01 11:07:04 +0300

    Raising ParseException for interactions with fewer than 2 words

--- src/actions/parsers/interact/InteractActionParser.py
+++ src/actions/parsers/interact/InteractActionParser.py
@@ -1,14 +1,19 @@
 import typing
 from src.actions.parsers.abstract.IActionParser import IActionParser
+from src.actions.parsers.ParseException import ParseException
 from src.actions.data_types.interaction.InteractionData import InteractionData
 
 
 class InteractActionParser(IActionParser):
     @staticmethod
     def GetWords() -> typing.List[str]:
-        raise NotImplementedError("InteractActionParser: Verbs evaluated for match in ActionHandler")
+        raise AttributeError("InteractActionParser: Verbs evaluated for match in InteractActionHandler!")
 
     @staticmethod
     def ParseToData(string: str) -> any:
         words = string.split(' ')
-        return InteractionData(words[0], " ".join(words[1:]))
+        if len(words) < 2:
+            raise ParseException("Unable to parse interaction {}.".format(string))
+        verb = words[0].lower()
+        noun = " ".join(words[1:]).lower()
+        return InteractionData(verb, noun)
